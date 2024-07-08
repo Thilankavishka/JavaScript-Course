99baf5c329bc83872f57552d90e8584ccd707361
Thilankavishka <thilankavishka25@gmail.com>
2024-07-08 21:00:53 +0530

    loops-while loop

--- Scripts/loops.js
+++ Scripts/loops.js
@@ -9,3 +9,18 @@ const x = 10;
 for (let i = 1; i <= x; i++) {
   console.log(i);
 }
+
+let i = 1;
+i++;
+console.log(i++); //post increment
+
+let j = 1;
+++j;
+console.log(++j); //pre increment
+
+//-------------------------------------------while loop-----------------------------------------------------------------------
+let z = 1;
+while (z < 5) {
+  console.log(z);
+  z++;
+}
